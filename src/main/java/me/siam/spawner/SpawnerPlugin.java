0b9f29e9cd9673e3b8a2c01d0def7ca290da723c
siam394 <nurulislamnubd339@gmail.com>
2026-02-17 21:57:15 +0600

    Add economy setup with Vault integration

--- src/main/java/me/siam/spawner/SpawnerPlugin.java
+++ src/main/java/me/siam/spawner/SpawnerPlugin.java
@@ -1,28 +1,51 @@
 package me.siam.spawner;
 
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SpawnerPlugin extends JavaPlugin {
 
     private static SpawnerPlugin instance;
     private SpawnerItem spawnerItem;
+    private static Economy econ;
 
     @Override
     public void onEnable() {
 
         instance = this;
 
+        if (!setupEconomy()) {
+            getLogger().severe("Vault not found! Disabling plugin...");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
         spawnerItem = new SpawnerItem(this);
 
         Bukkit.getPluginManager().registerEvents(new SpawnerListener(this), this);
 
+        new SpawnerGenerator(this);
+
         getLogger().info("SpawnerPlugin Enabled Successfully!");
     }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("SpawnerPlugin Disabled.");
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager()
+                .getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
     }
 
     public static SpawnerPlugin getInstance() {
