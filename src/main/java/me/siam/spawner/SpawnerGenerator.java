af4f366bfc92895a106e3ec7733576632e7c1d2d
siam394 <nurulislamnubd339@gmail.com>
2026-02-17 21:53:26 +0600

    Implement SpawnerGenerator for mob spawning management

---
+++ src/main/java/me/siam/spawner/SpawnerGenerator.java
@@ -0,0 +1,77 @@
+package me.siam.spawner;
+
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class SpawnerGenerator {
+
+    private final SpawnerPlugin plugin;
+    private final NamespacedKey mobKey;
+    private final NamespacedKey amountKey;
+    private final NamespacedKey xpKey;
+    private final NamespacedKey lootKey;
+
+    public SpawnerGenerator(SpawnerPlugin plugin) {
+        this.plugin = plugin;
+        this.mobKey = new NamespacedKey(plugin, "mobType");
+        this.amountKey = new NamespacedKey(plugin, "stackAmount");
+        this.xpKey = new NamespacedKey(plugin, "storedXP");
+        this.lootKey = new NamespacedKey(plugin, "storedLoot");
+
+        startTask();
+    }
+
+    private void startTask() {
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+
+                for (World world : Bukkit.getWorlds()) {
+
+                    world.getLoadedChunks();
+
+                    for (var chunk : world.getLoadedChunks()) {
+
+                        for (Block block : chunk.getTileEntities()) {
+
+                            if (!(block.getState() instanceof CreatureSpawner spawner)) continue;
+
+                            if (!spawner.getPersistentDataContainer().has(mobKey, PersistentDataType.STRING))
+                                continue;
+
+                            String mobName = spawner.getPersistentDataContainer().get(mobKey, PersistentDataType.STRING);
+                            int stackAmount = spawner.getPersistentDataContainer().get(amountKey, PersistentDataType.INTEGER);
+
+                            MobType type = MobType.valueOf(mobName);
+
+                            int generatedMobs = stackAmount * 2;
+
+                            int xpStored = spawner.getPersistentDataContainer().getOrDefault(
+                                    xpKey, PersistentDataType.INTEGER, 0);
+
+                            int lootStored = spawner.getPersistentDataContainer().getOrDefault(
+                                    lootKey, PersistentDataType.INTEGER, 0);
+
+                            xpStored += generatedMobs * type.getXpPerMob();
+                            lootStored += generatedMobs;
+
+                            spawner.getPersistentDataContainer().set(xpKey,
+                                    PersistentDataType.INTEGER, xpStored);
+
+                            spawner.getPersistentDataContainer().set(lootKey,
+                                    PersistentDataType.INTEGER, lootStored);
+
+                            spawner.update();
+                        }
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 100, 100); // 5 seconds
+    }
+}
