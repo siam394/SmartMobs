24f5e8cddae47563aad91377ac71814df911e0b6
siam394 <nurulislamnubd339@gmail.com>
2026-02-21 22:17:29 +0600

    Create SpawnerCommand.java

---
+++ src/main/java/me/siam/spawner/SpawnerCommand.java
@@ -0,0 +1,30 @@
+package me.siam.spawner;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SpawnerCommand implements CommandExecutor {
+
+    private final SpawnerPlugin plugin;
+
+    public SpawnerCommand(SpawnerPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        player.sendMessage("§aSpawner plugin working successfully!");
+
+        return true;
+    }
+}
